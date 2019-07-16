3c4b983b120599fdba28bd302bd0d1b97ebc3c62
murphylee2048 <1134697131@qq.com>
2019-07-16 17:47:58 +0800

    [ML]feat:should_return_employ_find_by_list

--- src/test/java/com/tw/apistackbase/ApiStackBaseApplicationTests.java
+++ src/test/java/com/tw/apistackbase/ApiStackBaseApplicationTests.java
@@ -1,14 +1,18 @@
 package com.tw.apistackbase;
 
 import com.tw.apistackbase.model.Employee;
+import com.tw.apistackbase.service.EmployService;
 import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class ApiStackBaseApplicationTests {
+	@MockBean
+	private EmployService employService;
 
 	@Test
 	public void contextLoads() {
@@ -16,11 +20,9 @@ public class ApiStackBaseApplicationTests {
 	}
 
 	@Test
-	public void should_return_all_list() {
-		// given
+	public void should_return_employ_find_by_list() {
 		Employee employee = new Employee("5", "sam", 22, "female");
 
-		// when
 	}
 
 }
